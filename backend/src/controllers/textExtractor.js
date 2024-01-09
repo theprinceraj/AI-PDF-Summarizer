b332877150e8a3b7ba407cd4854468a1a6b4b0c3
Prince Raj <profile.princeraj+github@gmail.com>
2024-01-09 10:31:45 +0530

    Update dependencies and add text extraction functionality.

--- backend/src/controllers/textExtractor.js
+++ backend/src/controllers/textExtractor.js
@@ -1,3 +1,13 @@
-export default async function extractTextFromFile(pdf) {
+import { createWorker } from 'tesseract.js';
+
+export async function extractTextFromImage(imageBuffer) {
+    const worker = await createWorker('eng');
+    const { data: { text } } = await worker.recognize(imageBuffer);
+    await worker.terminate();
+    console.log(text);
+    return text;
+}
+
+export async function extractTextFromPDF(pdf) {
 
 }
