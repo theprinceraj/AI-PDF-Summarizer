df95c67e7000265d9e0c6bb56a4e746cb73f169e
Prince Raj <profile.princeraj+github@gmail.com>
2024-01-09 11:18:36 +0530

    Update text extraction routes and controllers

--- backend/src/routes/textExtract.js
+++ backend/src/routes/textExtract.js
@@ -1,7 +1,7 @@
 import express from "express";
 const router = express.Router();
 
-import { textExtractionController } from "../controllers/textExtractor";
+import { textExtractionController } from "../controllers/textExtractionController";
 router.get("/", textExtractionController);
 
 export default router;
--- backend/src/utilities/imageToText.js
+++ backend/src/utilities/imageToText.js
@@ -1,13 +1,9 @@
 import { createWorker } from 'tesseract.js';
 
-export async function extractTextFromImage(imageBuffer) {
+export default async function extractTextFromImage(imageBuffer) {
     const worker = await createWorker('eng');
     const { data: { text } } = await worker.recognize(imageBuffer);
     await worker.terminate();
     console.log(text);
     return text;
-}
-
-export async function extractTextFromPDF(pdf) {
-
 }
